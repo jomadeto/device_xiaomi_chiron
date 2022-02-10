282a3344d092a5c7a15628a96e377f59fd01b54d
jhenrique09 <jhenrique09@users.noreply.github.com>
2022-02-10 20:45:19 +0100

    chiron: Enable quick tap support
    Change-Id: I86fe3b3ad892571f8ccc797b108dffc17019a6c9

--- lineage_chiron.mk
+++ lineage_chiron.mk
@@ -36,3 +36,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="chiron-user 8.0.0 OPR1.170623.027 V9.5.4.0.ODEMIFA release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/chiron/chiron:8.0.0/OPR1.170623.027/V9.5.4.0.ODEMIFA:user/release-keys
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
